d93fd6b9d971ce122de84da884fc418d92dad983
Takashi Ishio <ishio@is.naist.jp>
2018-01-31 17:33:14 +0900

    Added a test class for #1.

---
+++ trunk/tests/selogger/testdata/ClassLoaderMain.java
@@ -0,0 +1,69 @@
+package selogger.testdata;
+
+import java.io.IOException;
+import java.net.URL;
+
+import org.objectweb.asm.ClassReader;
+
+/**
+ * Test case for #1.
+ * Executing the main method with selogger resulted in a broken ObjectTypes file. 
+ */
+public class ClassLoaderMain {
+	
+	public static class CustomizedCLassLoader extends ClassLoader {
+		
+		/**
+		 * Create a class C whose getClassLoader() returns this custom class loader instance. 
+		 */
+		@Override
+		public Class<?> loadClass(String name) throws ClassNotFoundException {
+			try {
+				if (name.startsWith("java.")) return super.loadClass(name);
+				ClassReader r = new ClassReader(name);
+				System.out.println(name);
+				Class<?> c = defineClass(name, r.b, 0, r.b.length);
+				return c;
+			} catch (IOException e) {
+				return null;
+			}
+		}
+		
+		/**
+		 * Override a method used by TypeIdMap.
+		 * The class loads a class and make a new instance.
+		 * Consequently, selogger has to generate a new type ID and 
+		 * a new object ID for the instance.
+		 */
+		@Override
+		public URL getResource(String name) {
+			B b = new B();
+			if (name.startsWith("java.")) return super.getResource(name);
+			else return null;
+		}
+		
+	}
+
+	public static class A {
+		public A() {
+			
+		}
+	}
+	
+	public static class B {
+		
+	}
+	
+	public static void main(String[] args) {
+		CustomizedCLassLoader cl = new CustomizedCLassLoader();
+		try {
+			Class<?> c = cl.loadClass("selogger.testdata.ClassLoaderMain$A");
+			Object o = c.newInstance();
+			System.out.println(o.toString());
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+}
