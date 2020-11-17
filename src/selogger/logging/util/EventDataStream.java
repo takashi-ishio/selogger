c448178b007bed504164c923f21e1063efe782ea
Takashi Ishio <ishio@is.naist.jp>
2020-11-17 18:17:54 +0900

    Added javadoc

--- src/selogger/logging/util/EventDataStream.java
+++ src/selogger/logging/util/EventDataStream.java
@@ -8,9 +8,24 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 import selogger.logging.IErrorLogger;
 
+/**
+ * This class is a stream specialized to write a sequence of events into files.
+ * A triple of data ID, thread ID, and a value observed in the event is recorded.
+ *
+ * While a regular stream like FileOutputStream generates a single file,
+ * this stream creates a number of files whose size is limited by the number of events
+ * (MAX_EVENTS_PER_FILE field).
+ */
 public class EventDataStream {
 	
+	/**
+	 * The number of events stored in a single file.
+	 */
 	public static final int MAX_EVENTS_PER_FILE = 10000000;
+	
+	/**
+	 * The data size of an event.
+	 */
 	public static final int BYTES_PER_EVENT = 16;
 	
 	private FileNameGenerator files;
@@ -18,7 +33,14 @@ public class EventDataStream {
 	private IErrorLogger err;
 	private int count;
 
+	/**
+	 * This object records the number of threads observed by SELogger.
+	 */
 	private static final AtomicInteger nextThreadId = new AtomicInteger(0);
+	
+	/**
+	 * Assign an integer to this thread. 
+	 */
 	private static ThreadLocal<Integer> threadId = new ThreadLocal<Integer>() {
 		@Override
 		protected Integer initialValue() {
@@ -26,6 +48,11 @@ public class EventDataStream {
 		}
 	};
 	
+	/**
+	 * Create an instance of stream.
+	 * @param target is an object generating file names.
+	 * @param logger is to report errors that occur in this class.
+	 */
 	public EventDataStream(FileNameGenerator target, IErrorLogger logger) {
 		try {
 			files = target;
@@ -37,6 +64,11 @@ public class EventDataStream {
 		}
 	}
 	
+	/**
+	 * Write an event data into a file.  The thread ID is also recorded. 
+	 * @param dataId specifies an event and its bytecode location.
+	 * @param value specifies a data value observed in the event.
+	 */
 	public synchronized void write(int dataId, long value) {
 		if (out != null) {
 			try {
@@ -56,6 +88,9 @@ public class EventDataStream {
 		}
 	}
 	
+	/**
+	 * Close the stream.
+	 */
 	public synchronized void close() {
 		try {
 			out.close();
