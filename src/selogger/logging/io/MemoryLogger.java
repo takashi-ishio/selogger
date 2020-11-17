a890e70f33999b3cf5f02b1424350328623d3950
Takashi Ishio <ishio@is.naist.jp>
2020-11-17 22:26:56 +0900

    Simplified code, added Javadoc

--- src/selogger/logging/io/MemoryLogger.java
+++ src/selogger/logging/io/MemoryLogger.java
@@ -5,71 +5,120 @@ import java.util.ArrayList;
 import selogger.logging.IEventLogger;
 
 /**
- * A logger records all events on memory for testing selogger features.
+ * A logger records all events on memory for testing SELogger features.
  */
 public class MemoryLogger implements IEventLogger {
 
+	/**
+	 * An event object to keep a data ID and an observed value.
+	 */
 	public static class Event {
+		
 		private int dataId;
 		private Class<?> valueType;
 		private Object objectValue;
 		private long value;
 		
+		/**
+		 * Create an event instance using a primitive value.
+		 * @param dataId
+		 * @param type specifies the original type of the value.
+		 * @param value 
+		 */
 		public Event(int dataId, Class<?> type, long value) {
 			this.dataId = dataId;
 			this.valueType = type;
 			this.value = value;
 		}
 
-		public Event(int dataId, Class<?> type, Object value) {
+		/**
+		 * Create an event instance using an Object value.
+		 * @param dataId
+		 * @param value
+		 */
+		public Event(int dataId, Object value) {
 			this.dataId = dataId;
-			this.valueType = type;
+			if (value == null) {
+				this.valueType = Object.class;
+			} else {
+				this.valueType = value.getClass();
+			}
 			this.objectValue = value;
 		}
 		
+		/**
+		 * @return data ID of the event.
+		 */
 		public int getDataId() {
 			return dataId;
 		}
 		
+		/**
+		 * @return the data type of the recorded value.
+		 */
 		public Class<?> getValueType() {
 			return valueType;
 		}
 		
+		/**
+		 * @return the data value as a boolean value.
+		 */
 		public boolean getBooleanValue() {
 			assert valueType.equals(boolean.class);
 			return value != 0;
 		}
 		
+		/**
+		 * @return the data value as a byte value.
+		 */
 		public byte getByteValue() {
 			assert valueType.equals(byte.class);
 			return (byte)value;
 		}
 		
+		/**
+		 * @return the data value as a char value.
+		 */
 		public char getCharValue() {
 			assert valueType.equals(char.class);
 			return (char)value;
 		}
 		
+		/**
+		 * @return the data value as a short value.
+		 */
 		public short getShortValue() {
 			assert valueType.equals(short.class);
 			return (short)value;
 		}
 		
+		/**
+		 * @return the data value as an int value.
+		 */
 		public int getIntValue() {
 			assert valueType.equals(int.class);
 			return (int)value;
 		}
 		
+		/**
+		 * @return the data value as a long value.
+		 */
 		public long getLongValue() {
 			assert valueType.equals(long.class);
 			return (long)value;
 		}
 		
+		/**
+		 * @return the data value as a float value.
+		 */
 		public float getFloatValue() {
 			assert valueType.equals(float.class);
 			return Float.intBitsToFloat((int)value);
 		}
 
+		/**
+		 * @return the data value as a double value.
+		 */
 		public double getDoubleValue() {
 			assert valueType.equals(double.class);
 			return Double.longBitsToDouble(value);
@@ -84,61 +133,96 @@ public class MemoryLogger implements IEventLogger {
 	
 	private ArrayList<Event> events;
 	
+	
+	/**
+	 * Create an instance of this logging class.
+	 * This does not require any parameter.
+	 */
 	public MemoryLogger() {
 		events = new ArrayList<>();
 	}
 	
+	/**
+	 * @return recorded events.
+	 */
 	public ArrayList<Event> getEvents() {
 		return events;
 	}
 	
+	/**
+	 * Close the stream.  This method actually does nothing.
+	 */
 	@Override
 	public void close() {
 	}
-	
+
+	/**
+	 * Record an event on memory. 
+	 */
 	@Override
 	public void recordEvent(int dataId, boolean value) {
 		events.add(new Event(dataId, boolean.class, value? 1: 0));
 	}
 	
+	/**
+	 * Record an event on memory. 
+	 */
 	@Override
 	public void recordEvent(int dataId, byte value) {
 		events.add(new Event(dataId, byte.class, value));
 	}
 	
+	/**
+	 * Record an event on memory. 
+	 */
 	@Override
 	public void recordEvent(int dataId, char value) {
 		events.add(new Event(dataId, char.class, value));
 	}
 	
+	/**
+	 * Record an event on memory. 
+	 */
+	@Override
 	public void recordEvent(int dataId, double value) {
 		events.add(new Event(dataId, double.class, Double.doubleToLongBits(value)));
 	}
 
+	/**
+	 * Record an event on memory. 
+	 */
 	@Override
 	public void recordEvent(int dataId, float value) {
 		events.add(new Event(dataId, float.class, Float.floatToIntBits(value)));
 	}
 	
+	/**
+	 * Record an event on memory. 
+	 */
 	@Override
 	public void recordEvent(int dataId, int value) {
 		events.add(new Event(dataId, int.class, value));
 	}
 
+	/**
+	 * Record an event on memory. 
+	 */
 	@Override
 	public void recordEvent(int dataId, long value) {
 		events.add(new Event(dataId, long.class, value));
 	}
 	
+	/**
+	 * Record an event on memory. 
+	 */
 	@Override
 	public void recordEvent(int dataId, Object value) {
-		if (value == null) {
-			events.add(new Event(dataId, Object.class, value));
-		} else {
-			events.add(new Event(dataId, value.getClass(), value));
-		}
+		events.add(new Event(dataId, value));
 	}
 	
+	/**
+	 * Record an event on memory. 
+	 */
 	@Override
 	public void recordEvent(int dataId, short value) {
 		events.add(new Event(dataId, short.class, value));
