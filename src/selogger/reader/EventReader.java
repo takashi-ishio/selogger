cd06949dc981eb1289da289b2afa3b56463d0dbf
Takashi Ishio <ishio@is.naist.jp>
2020-11-25 21:59:25 +0900

    Fixed: Calling readSubevents twice might cause an error

--- src/selogger/reader/EventReader.java
+++ src/selogger/reader/EventReader.java
@@ -12,21 +12,34 @@ import selogger.EventType;
 import selogger.logging.io.EventStreamLogger;
 import selogger.logging.util.EventDataStream;
 
-
+/**
+ * This class is to read a sequence of events from .slg files.
+ */
 public class EventReader {
 	
 	private static int bufferSize = EventDataStream.BYTES_PER_EVENT * EventDataStream.MAX_EVENTS_PER_FILE;
 
 	private File[] logFiles;
-	protected ObjectTypeMap objectTypeMap;
-	protected long nextEventId;
-	protected boolean processParams;
 	protected ByteBuffer buffer;
 	private int fileIndex;
+
+	protected long nextEventId;
+	protected ObjectTypeMap objectTypeMap;
+	protected boolean processParams;
 	private DataIdMap dataIdMap;
+
+
+	/**
+	 * Events created for some reasons but not yet returned to a client.
+	 */
 	private LinkedList<Event> unprocessed;
 	
 
+	/**
+	 * Create an instance for reading files from a specified directory.
+	 * @param dir is a directory containing *.slg files.
+	 * @param dataIdMap is a DataIdMap to analyze the *.slg files.
+	 */
 	public EventReader(File dir, DataIdMap dataIdMap) {
 		this.logFiles =  SequentialFileList.getSortedList(dir, EventStreamLogger.LOG_PREFIX, EventStreamLogger.LOG_SUFFIX);
 		this.dataIdMap = dataIdMap;
@@ -41,13 +54,19 @@ public class EventReader {
 	 * Associate a entry/call event with its parameter events.
 	 * This option may miss some parameter events 
 	 * if events are not sequential because of multi-threading.   
-	 * @param processParams
+	 * @param processParams If true, this reader automatically links parameter events. 
 	 */
 	public void setProcessParams(boolean processParams) {
 		this.processParams = processParams;
 	}
 	
+	/**
+	 * Load a file from a file.
+	 * @return true if a file is successfully loaded.
+	 * False indicates that no more files exist or an error occurred.  
+	 */
 	protected boolean load() {
+		// This method fails if no more files 
 		if (fileIndex >= logFiles.length) {
 			buffer.clear();
 			buffer.flip();
@@ -66,18 +85,6 @@ public class EventReader {
 			buffer.flip();
 			return false;
 		}
-	}
-	
-	public void cancelRead(Event e) {
-		for (ListIterator<Event> it = unprocessed.listIterator(); it.hasNext(); ) {
-			Event u = it.next();
-			if (u.getEventId() > e.getEventId()) {
-				it.previous();
-				it.add(e);
-				return;
-			}
-		}
-		unprocessed.addLast(e);
 	}
 	
 	
@@ -112,17 +119,26 @@ public class EventReader {
 	}
 	
 	
+	/**
+	 * Read the next event from a stream of events
+	 * @return an event if exists.  This method returns null at the end of the files.
+	 */
 	public Event nextEvent() {
 		Event e = (unprocessed.size() > 0) ? unprocessed.removeFirst() : readEventFromBuffer(); 
 		if (e != null && processParams) readSubevents(e);
 		return e;
 	}
 	
-	protected void readSubevents(Event e) {
-		if (e == null) return;  // end of event stream
+	/**
+	 * Read parameter events and link them to the main event.
+	 * @param main is the event that can have parameters 
+	 */
+	protected void readSubevents(Event main) {
+		if (main == null) return;  // end of event stream
+		if (main.getParams() != null) return; // already processed
 		
 		EventType sub;
-		switch (e.getEventType()) {
+		switch (main.getEventType()) {
 		case METHOD_ENTRY:
 			sub = EventType.METHOD_PARAM;
 			break;
@@ -133,20 +149,22 @@ public class EventReader {
 			sub = EventType.INVOKE_DYNAMIC_PARAM;
 			break;
 		default:
+			// the event has no params
 			return;
 		}
 
-		// Load following parameter events
-		int paramCount = e.getParamCount();
+		// Load successive events until parameter events are found
+		// Parameter events may not be consecutive due to multi-threading
+		int paramCount = main.getParamCount();
 		Event[] params = new Event[paramCount];
-		e.setParams(params);
+		main.setParams(params);
 		int count = 0;
-		Event candidate = e;
+		Event candidate = main;
 		while (count < paramCount) {
 			candidate = nextEvent();
 			if (candidate == null) { // end of trace
 				break;
-			} else if (candidate.getThreadId() == e.getThreadId() && 
+			} else if (candidate.getThreadId() == main.getThreadId() && 
 					candidate.getEventType() == sub) {
 				params[count++] = candidate;
 			} else { // if params are not recorded 
@@ -156,6 +174,12 @@ public class EventReader {
 		}
 	}
 
+	/**
+	 * Read an event from the internal byte buffer.
+	 * If the buffer is empty, this method loads the next file.
+	 * @return a new event object.  
+	 * Null is returned if the method reached the end of files.
+	 */
 	protected Event readEventFromBuffer() {
 		// try to read the next event from a stream.
 		while (buffer != null && buffer.remaining() == 0) {
@@ -172,9 +196,9 @@ public class EventReader {
 
 
 	/**
-	 * Move to a paritcular event.
+	 * Move to a particular event.
 	 * The specified eventId is obtained by the next readEvent call.
-	 * @param eventId
+	 * @param eventId specifies the event location.
 	 */
 	public void seek(long eventId) {
 		if (eventId == nextEventId) return;
@@ -188,5 +212,23 @@ public class EventReader {
 		buffer.position(pos);
 		nextEventId = eventId;
 	}
+
+	/**
+	 * Put an event object back to the reader.
+	 * @param e is an event to be put back.
+	 */
+	public void cancelRead(Event e) {
+		// Search an appropriate event location using the event ID.
+		for (ListIterator<Event> it = unprocessed.listIterator(); it.hasNext(); ) {
+			Event u = it.next();
+			if (u.getEventId() > e.getEventId()) {
+				it.previous();
+				it.add(e);
+				return;
+			}
+		}
+		unprocessed.addLast(e);
+	}
 	
+
 }
