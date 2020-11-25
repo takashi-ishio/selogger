b526f33dffab16408b801a31e51461f49de46de6
Takashi Ishio <ishio@is.naist.jp>
2020-11-25 13:52:09 +0900

    Added comment

--- src/selogger/weaver/MetracerClassWriter.java
+++ src/selogger/weaver/MetracerClassWriter.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/**
+ * This code is imported to SELogger to avoid invalid bytecode
+ */
 
 package selogger.weaver;
 
