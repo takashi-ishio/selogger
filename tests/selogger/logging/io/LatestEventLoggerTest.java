e8836e3ae3ad5b451df2c13fbeef9fbac7717f4d
Takashi Ishio <ishio@is.naist.jp>
2022-05-26 13:19:03 +0900

    Added a missing parameter

--- tests/selogger/logging/io/LatestEventLoggerTest.java
+++ tests/selogger/logging/io/LatestEventLoggerTest.java
@@ -4,12 +4,31 @@ package selogger.logging.io;
 import org.junit.Assert;
 import org.junit.Test;
 
+import selogger.logging.IErrorLogger;
 import selogger.logging.io.LatestEventLogger.ObjectRecordingStrategy;
 import selogger.logging.util.ObjectIdFile.ExceptionRecording;
 
 
 public class LatestEventLoggerTest {
 
+	/**
+	 * An implementation of IErrorLogger that simply discards events for testing
+	 */
+	private static class EmptyErrorLogger implements IErrorLogger {
+
+		@Override
+		public void log(String msg) {
+		}
+		
+		@Override
+		public void log(Throwable t) {
+		}
+		
+		@Override
+		public void close() {
+		}
+
+	}
 
 	/**
 	 * LatestEventTimeLogger with additional methods for testing
@@ -17,7 +36,7 @@ public class LatestEventLoggerTest {
 	private static class LatestEventLoggerForTest extends LatestEventLogger {
 		
 		public LatestEventLoggerForTest(int size, ObjectRecordingStrategy keepObject) {
-			super(null, size, keepObject, false, ExceptionRecording.Disabled, false);
+			super(null, size, keepObject, false, ExceptionRecording.Disabled, false, new EmptyErrorLogger());
 		}
 		
 		/**
