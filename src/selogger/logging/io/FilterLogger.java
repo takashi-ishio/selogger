db51033cbff43ae53c56772d073cfe9cc05485f3
Takashi Ishio <ishio@is.naist.jp>
2022-06-01 10:39:20 +0900

    Added test cases

--- src/selogger/logging/io/FilterLogger.java
+++ src/selogger/logging/io/FilterLogger.java
@@ -12,6 +12,13 @@ public class FilterLogger implements IEventLogger {
 	private IErrorLogger errorLogger;
 	private boolean enabled;
 	
+	/**
+	 * A filter object to record only events between START and END.
+	 * @param mainLogger is a logger object receiving filtered events 
+	 * @param start specifies events that enable logging
+	 * @param end specifies events that disable logging
+	 * @param errorLogger is an error message recorder
+	 */
 	public FilterLogger(IEventLogger mainLogger, ILoggingTarget start, ILoggingTarget end, IErrorLogger errorLogger) {
 		this.mainLogger = mainLogger;
 		this.start = start;
@@ -20,74 +27,116 @@ public class FilterLogger implements IEventLogger {
 		this.enabled = false;
 	}
 	
+	/**
+	 * @return true if the logging is enabled
+	 */
+	public boolean isEnabled() {
+		return enabled;
+	}
+	
+	/**
+	 * Enable/Disable the logging according to an observed event.
+	 * @param dataId is the dataId of the observed event.
+	 * @return true if the dataId is an end event.  This flag is needed to record the event. 
+	 */
 	private boolean updateStatus(int dataId) {
 		boolean disabled = false;
+		if (start.isTarget(dataId)) {
+			this.enabled = true;
+			errorLogger.log("FilterLogger:enabled dataId=" + dataId);
+		}
 		if (enabled && end.isTarget(dataId)) {
 			disabled = true;
 			this.enabled = false;
 			errorLogger.log("FilterLogger:disabled dataId=" + dataId);
 		}
-		if (start.isTarget(dataId)) {
-			this.enabled = true;
-			errorLogger.log("FilterLogger:enabled dataId=" + dataId);
-		}
 		return disabled;
 	}
 
+	/**
+	 * Record an event if the logging is enabled
+	 */
 	@Override
 	public void recordEvent(int dataId, boolean value) {
 		boolean disabledOnThisEvent = updateStatus(dataId);
 		if (enabled || disabledOnThisEvent) mainLogger.recordEvent(dataId, value);
 	}
 	
+	/**
+	 * Record an event if the logging is enabled
+	 */
 	@Override
 	public void recordEvent(int dataId, byte value) {
 		boolean disabledOnThisEvent = updateStatus(dataId);
 		if (enabled || disabledOnThisEvent) mainLogger.recordEvent(dataId, value);
 	}
 	
+	/**
+	 * Record an event if the logging is enabled
+	 */
 	@Override
 	public void recordEvent(int dataId, char value) {
 		boolean disabledOnThisEvent = updateStatus(dataId);
 		if (enabled || disabledOnThisEvent) mainLogger.recordEvent(dataId, value);
 	}
 	
+	/**
+	 * Record an event if the logging is enabled
+	 */
 	@Override
 	public void recordEvent(int dataId, double value) {
 		boolean disabledOnThisEvent = updateStatus(dataId);
 		if (enabled || disabledOnThisEvent) mainLogger.recordEvent(dataId, value);
 	}
 	
+	/**
+	 * Record an event if the logging is enabled
+	 */
 	@Override
 	public void recordEvent(int dataId, float value) {
 		boolean disabledOnThisEvent = updateStatus(dataId);
 		if (enabled || disabledOnThisEvent) mainLogger.recordEvent(dataId, value);
 	}
 	
+	/**
+	 * Record an event if the logging is enabled
+	 */
 	@Override
 	public void recordEvent(int dataId, int value) {
 		boolean disabledOnThisEvent = updateStatus(dataId);
 		if (enabled || disabledOnThisEvent) mainLogger.recordEvent(dataId, value);
 	}
 	
+	/**
+	 * Record an event if the logging is enabled
+	 */
 	@Override
 	public void recordEvent(int dataId, long value) {
 		boolean disabledOnThisEvent = updateStatus(dataId);
 		if (enabled || disabledOnThisEvent) mainLogger.recordEvent(dataId, value);
 	}
 	
+	/**
+	 * Record an event if the logging is enabled
+	 */
 	@Override
 	public void recordEvent(int dataId, Object value) {
 		boolean disabledOnThisEvent = updateStatus(dataId);
 		if (enabled || disabledOnThisEvent) mainLogger.recordEvent(dataId, value);
 	}
 	
+	/**
+	 * Record an event if the logging is enabled
+	 */
 	@Override
 	public void recordEvent(int dataId, short value) {
 		boolean disabledOnThisEvent = updateStatus(dataId);
 		if (enabled || disabledOnThisEvent) mainLogger.recordEvent(dataId, value);
 	}
 	
+	/**
+	 * This method closes the main logger object.
+	 */
 	@Override
 	public void close() {
 		mainLogger.close();
---
+++ tests/selogger/logging/io/FilterLoggerTest.java
@@ -0,0 +1,122 @@
+package selogger.logging.io;
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import selogger.logging.IErrorLogger;
+import selogger.logging.ILoggingTarget;
+
+public class FilterLoggerTest {
+
+	/**
+	 * A target object to directly specify a dataId for testing
+	 */
+	public static class FixedId implements ILoggingTarget {
+		
+		private int target = 0;
+		
+		public FixedId(int target) {
+			this.target = target;
+		}
+		@Override
+		public boolean isTarget(int dataid) {
+			return dataid == target;
+		}
+	}
+	
+	/**
+	 * A message recorder for testing
+	 */
+	public static class StringLogger implements IErrorLogger {
+		private ArrayList<String> messages;
+		public StringLogger() {
+			messages = new ArrayList<>();
+		}
+		@Override
+		public void log(String msg) {
+			messages.add(msg);
+		}
+		@Override
+		public void log(Throwable t) {
+		}
+		@Override
+		public void close() {
+		}
+		
+		public int getMessageCount() {
+			return messages.size();
+		}
+	}
+
+	@Test
+	public void testFilter() {
+		MemoryLogger mem = new MemoryLogger();
+		StringLogger messages = new StringLogger();
+		FilterLogger filter = new FilterLogger(mem, new FixedId(1), new FixedId(3), messages);
+		
+		// At first, logging is disabled 
+		Assert.assertFalse(filter.isEnabled());
+		filter.recordEvent(0, 0);
+		Assert.assertFalse(filter.isEnabled());
+		
+		// The first start event enables the logging 
+		filter.recordEvent(1, 0);
+		Assert.assertTrue(filter.isEnabled());
+
+		filter.recordEvent(2, 0);
+		filter.recordEvent(1, 0);
+
+		// The end event disables the logging 
+		filter.recordEvent(3, 0);
+		Assert.assertFalse(filter.isEnabled());
+
+		// This event is ignored
+		filter.recordEvent(4, 0);
+		
+		// Four events between 1 and 3 are recorded 
+		Assert.assertEquals(4, mem.getEvents().size());
+		Assert.assertEquals(1, mem.getEvents().get(0).getDataId());
+		Assert.assertEquals(2, mem.getEvents().get(1).getDataId());
+		Assert.assertEquals(1, mem.getEvents().get(2).getDataId());
+		Assert.assertEquals(3, mem.getEvents().get(3).getDataId());
+	}
+
+	@Test
+	public void testEnableAndDisable() {
+		MemoryLogger mem = new MemoryLogger();
+		StringLogger messages = new StringLogger();
+		FilterLogger filter = new FilterLogger(mem, new FixedId(1), new FixedId(1), messages);
+		
+		// At first, logging is disabled
+		Assert.assertFalse(filter.isEnabled());
+		filter.recordEvent(0, 0);
+
+		// Logging stays disabled if the start and end events are the same,
+		// while it records the event itself
+		Assert.assertFalse(filter.isEnabled());
+		filter.recordEvent(1, 0);
+		Assert.assertFalse(filter.isEnabled());
+		
+		Assert.assertEquals(2, messages.getMessageCount());
+		Assert.assertEquals(1, mem.getEvents().size());
+		Assert.assertEquals(1, mem.getEvents().get(0).getDataId());
+
+		// This event is ignored
+		filter.recordEvent(2, 0);
+		Assert.assertFalse(filter.isEnabled());
+		Assert.assertEquals(2, messages.getMessageCount());
+
+		// The filter records the target event 
+		filter.recordEvent(1, 0);
+		Assert.assertFalse(filter.isEnabled());
+		Assert.assertEquals(4, messages.getMessageCount());
+		Assert.assertEquals(2, mem.getEvents().size());
+		Assert.assertEquals(1, mem.getEvents().get(1).getDataId());
+
+		// This event is ignored
+		filter.recordEvent(4, 0);
+		Assert.assertFalse(filter.isEnabled());
+	}
+}
