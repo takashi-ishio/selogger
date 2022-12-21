1592df19ae6adc010ad15bed1ee6b27642c83a5b
Takashi Ishio <ishio@is.naist.jp>
2022-12-21 12:19:01 +0900

    Added javadoc

--- src/selogger/logging/util/ObjectId.java
+++ src/selogger/logging/util/ObjectId.java
@@ -1,11 +1,22 @@
 package selogger.logging.util;
 
+/**
+ * This object represents an object ID.
+ * It comprises an identification number and a class name.
+ * It also keeps a string content if the class is either String or Throwable.
+ */
 public class ObjectId {
 
 	private long id;
 	private String className;
 	private String content;
 	
+	/**
+	 * Create an object ID.
+	 * @param id specifies an identification number.
+	 * @param className specifies a class name.
+	 * @param content specifies a textual content.
+	 */
 	public ObjectId(long id, String className, String content) {
 		this.id = id;
 		this.className = className;
