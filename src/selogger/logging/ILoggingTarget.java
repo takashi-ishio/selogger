309bae961b944afee2702e562733a3f345d5f769
Takashi Ishio <ishio@is.naist.jp>
2022-12-21 11:00:07 +0900

    Added javadoc

--- src/selogger/logging/ILoggingTarget.java
+++ src/selogger/logging/ILoggingTarget.java
@@ -1,6 +1,14 @@
 package selogger.logging;
 
+/**
+ * An interface to specify target events 
+ */
 public interface ILoggingTarget {
 
+	/**
+	 * This method is called for filtering events.
+	 * @param dataid represents an event.
+	 * @return whether a given event is a "target" event or not.  
+	 */
 	public boolean isTarget(int dataid);
 }
